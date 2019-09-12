d6dbd2520abaca746059f2835bd531279b5bbb28
Tiago Deliberali <tiago.deliberali@gmail.com>
2019-09-12 19:39:20 -0300

    Added routes

--- Finpe.React/src/App.js
+++ Finpe.React/src/App.js
@@ -3,13 +3,21 @@ import React, { Component } from "react";
 import { hot } from "react-hot-loader";
 import MenuBar from "./components/MenuBar"
 import Chart from "./components/Chart"
+import { Router, Link } from "@reach/router";
 
 class App extends Component {
     render() {
         return (
             <div className="App">
                 <MenuBar />
-                <Chart />
+                <nav>
+                    <Link to="/">Home</Link>{" "}
+                    <Link to="chart">chart</Link>{" "}
+                </nav>
+
+                <Router>
+                    <Chart path="/chart" />
+                </Router>
             </div>
         );
     }
