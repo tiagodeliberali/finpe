2b8c782e2bcce5b68a31b3ab49f592b4bf1fe8e5
Tiago Deliberali <tiago.deliberali@gmail.com>
2019-09-15 19:51:42 -0300

    Fixed typos

---
+++ Finpe.Api/Migrations/20190915_AddDifferenceColumn.cs
@@ -0,0 +1,19 @@
+﻿using FluentMigrator;
+
+namespace Finpe.Api.Migrations
+{
+    [Migration(20190915192600)]
+    public class AddDifferenceColumn : Migration
+    {
+        public override void Up()
+        {
+            Alter.Table("TransactionLine")
+                .AddColumn("Difference").AsDecimal();
+        }
+
+        public override void Down()
+        {
+            Delete.Column("Difference").FromTable("TransactionLine");
+        }
+    }
+}
