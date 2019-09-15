56c764eeb1666a889b62cc198b0c8080f22a4f51
Tiago Deliberali <tiago.deliberali@gmail.com>
2019-09-15 20:30:36 -0300

    Fixed migration

--- Finpe.Api/Migrations/20190914_InitialSchema.cs
+++ Finpe.Api/Migrations/20190914_InitialSchema.cs
@@ -20,8 +20,8 @@ namespace Finpe.Api.Migrations
                 .WithColumn("Amount").AsDecimal().NotNullable()
                 .WithColumn("StartYearMonth_Month").AsInt32().NotNullable()
                 .WithColumn("StartYearMonth_Year").AsInt32().NotNullable()
-                .WithColumn("EndYearMonth_Month").AsInt32()
-                .WithColumn("EndYearMonth_Year").AsInt32()
+                .WithColumn("EndYearMonth_Month").AsInt32().Nullable()
+                .WithColumn("EndYearMonth_Year").AsInt32().Nullable()
                 .WithColumn("Category").AsString(255).NotNullable()
                 .WithColumn("Responsible").AsString(255).NotNullable()
                 .WithColumn("Importance").AsString(255).NotNullable();
@@ -32,10 +32,11 @@ namespace Finpe.Api.Migrations
                 .WithColumn("TransactionDate").AsDateTime2().NotNullable()
                 .WithColumn("Amount").AsDecimal().NotNullable()
                 .WithColumn("Description").AsString(255).NotNullable()
-                .WithColumn("Category").AsString(255)
-                .WithColumn("Responsible").AsString(255)
-                .WithColumn("Importance").AsString(255)
-                .WithColumn("MultiCategoryTransactionLineID").AsInt64();
+                .WithColumn("Category").AsString(255).Nullable()
+                .WithColumn("Responsible").AsString(255).Nullable()
+                .WithColumn("Importance").AsString(255).Nullable()
+                .WithColumn("MultiCategoryTransactionLineID").AsInt64().Nullable()
+                .WithColumn("Difference").AsDecimal().Nullable();
 
             Create.ForeignKey("FK_MultiCategoryTransactionLine_TransactionLine")
                 .FromTable("TransactionLine").ForeignColumn("MultiCategoryTransactionLineID")
