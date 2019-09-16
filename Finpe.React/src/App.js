701a37d0069a17c2d1b133f29e4e9b4c9cb53f9f
Tiago Deliberali <tiago.deliberali@gmail.com>
2019-09-15 23:00:27 -0300

    Added budget form

--- Finpe.React/src/App.js
+++ Finpe.React/src/App.js
@@ -4,6 +4,7 @@ import { hot } from "react-hot-loader";
 import MenuBar from "./components/MenuBar"
 import Chart from "./components/Chart"
 import RecurrencyTransactionForm from "./components/RecurrencyTransactionForm"
+import BudgetForm from "./components/BudgetForm"
 import { Router, Link } from "@reach/router";
 
 class App extends Component {
@@ -12,13 +13,15 @@ class App extends Component {
             <div className="App">
                 <MenuBar />
                 <nav>
-                    <Link to="/add-recurrency">Home</Link>{" "}
-                    <Link to="/">chart</Link>{" "}
+                    <Link to="/">Início</Link>{" "}
+                    <Link to="/add-recurrency">Conta Recorrente</Link>{" "}
+                    <Link to="/add-budget">Conta Recorrente</Link>{" "}
                 </nav>
 
                 <Router>
-                    <RecurrencyTransactionForm path="/add-recurrency" />
                     <Chart path="/" />
+                    <RecurrencyTransactionForm path="/add-recurrency" />
+                    <BudgetForm path="/add-budget" />
                 </Router>
             </div>
         );
--- Finpe.React/src/utils/FinpeData.js
+++ Finpe.React/src/utils/FinpeData.js
@@ -1,10 +1,19 @@
-const baseUrl = process.env.API_BASE_URL || 'https://finpe-api-forno.azurewebsites.net/api/'
+const baseUrl = process.env.API_BASE_URL || 'https://localhost:44362/api/'
 
 export const fetchApiData = () => 
     fetch(baseUrl + 'MonthlyView');
 
 export const postRecurrency = (values) => 
     fetch(baseUrl + 'Recurrency', {
+        method: 'POST',
+        body: JSON.stringify(values),
+        headers: {
+            "Content-type": "application/json; charset=UTF-8"
+        }
+    })
+
+export const postBudget = (values) => 
+    fetch(baseUrl + 'Budget', {
         method: 'POST',
         body: JSON.stringify(values),
         headers: {
