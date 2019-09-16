002e06acf299f821cf7e6b20387bd5970fb07298
Tiago Deliberali <tiago.deliberali@gmail.com>
2019-09-16 18:13:23 -0300

    wip

--- Finpe.React/webpack.config.js
+++ Finpe.React/webpack.config.js
@@ -1,35 +1,45 @@
-
 const path = require("path");
 const webpack = require("webpack");
 
-module.exports = {
-  entry: "./src/index.js",
-  mode: "development",
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /(node_modules|bower_components)/,
-        loader: "babel-loader",
-        options: { presets: ["@babel/env"] }
-      },
-      {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"]
-      }
+module.exports = env => {
+
+  return {
+    entry: "./src/index.js",
+    mode: "development",
+    module: {
+      rules: [{
+          test: /\.(js|jsx)$/,
+          exclude: /(node_modules|bower_components)/,
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/env"]
+          }
+        },
+        {
+          test: /\.css$/,
+          use: ["style-loader", "css-loader"]
+        }
+      ]
+    },
+    resolve: {
+      extensions: ["*", ".js", ".jsx"]
+    },
+    output: {
+      path: path.resolve(__dirname, "public/dist/"),
+      publicPath: "/dist/",
+      filename: "bundle.js"
+    },
+    devServer: {
+      contentBase: path.join(__dirname, "public/"),
+      port: 3000,
+      publicPath: "http://localhost:3000/dist/",
+      hotOnly: true
+    },
+    plugins: [
+      new webpack.HotModuleReplacementPlugin(),
+      new webpack.DefinePlugin({ 
+        "API_BASE_URL": JSON.stringify("https://myapp-staging.firebaseio.com") 
+      })
     ]
-  },
-  resolve: { extensions: ["*", ".js", ".jsx"] },
-  output: {
-    path: path.resolve(__dirname, "public/dist/"),
-    publicPath: "/dist/",
-    filename: "bundle.js"
-  },
-  devServer: {
-    contentBase: path.join(__dirname, "public/"),
-    port: 3000,
-    publicPath: "http://localhost:3000/dist/",
-    hotOnly: true
-  },
-  plugins: [new webpack.HotModuleReplacementPlugin()]
+  }
 };
--- docker/front.Dockerfile
+++ docker/front.Dockerfile
@@ -6,13 +6,12 @@ COPY Finpe.React/package.json /app/package.json
 RUN npm install --silent
 RUN npm install react-scripts@3.0.1 -g --silent
 COPY Finpe.React/. /app
+ARG API_URL
 RUN npm run build
 
 # production environment
 FROM nginx:1.16.0-alpine AS runtime
 COPY --from=build /app/public /var/www
 COPY docker/nginx.conf /etc/nginx/nginx.conf
-ARG API_URL
-ENV API_BASE_URL $API_URL
 EXPOSE 80
 CMD ["nginx", "-g", "daemon off;"]
