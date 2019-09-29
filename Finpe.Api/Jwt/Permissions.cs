f9cf5241537fc2d7bb8d3f905127dbde868762e9
Tiago Deliberali <tiago.deliberali@gmail.com>
2019-09-28 22:08:08 -0300

    Refactored permissions

---
+++ Finpe.Api/Jwt/Permissions.cs
@@ -0,0 +1,8 @@
+﻿namespace Finpe.Api.Jwt
+{
+    public static class Permissions
+    {
+        public const string ViewAll = "view:all";
+        public const string WriteAll = "write:all";
+    }
+}
