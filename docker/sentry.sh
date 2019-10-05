dc57881b67e8414b5558c27f44759976f748c379
Tiago Deliberali <tiago.deliberali@gmail.com>
2019-10-04 22:39:24 -0300

    Fixed docker

---
+++ docker/sentry.sh
@@ -0,0 +1,19 @@
+# Create a new release
+
+echo 'Updating Sentry Project: finpe'
+echo 'New Release: '${DRONE_BRANCH}':'${DRONE_COMMIT_SHA}
+
+curl https://sentry.io/api/0/organizations/finpe/releases/ \
+  -X POST \
+  -H 'Authorization: Bearer '${SENTRY_AUTH_TOKEN} \
+  -H 'Content-Type: application/json' \
+  -d '
+  {
+    "version": "'${DRONE_COMMIT_SHA}'",
+    "refs": [{
+        "repository":"tiagodeliberali/finpe",
+        "commit":"'${DRONE_COMMIT_SHA}'"
+    }],
+    "projects":["finpe-api"]
+}
+'
